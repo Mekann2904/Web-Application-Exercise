1183d3773d7eb96bbb48161e0c6596b30cffeafe
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2024-07-28 00:03:10 +0900

    css,javascriptの修正

--- app/static/js/dbviewer.js
+++ app/static/js/dbviewer.js
@@ -11,25 +11,43 @@ window.onload = function() {
             });
 
             function updateTable() {
-                let tableBody = document.getElementById('shelter-table').getElementsByTagName('tbody')[0];
+                let table = document.getElementById('shelter-table');
+                table.classList.add('location-table'); // テーブルにクラスを追加
+                table.style.borderSpacing = '0px 5px'; // 行には隙間を開けず、列には5pxの隙間を設定
+                table.style.fontSize = 'larger'; // フォントサイズをlargerに設定
+                table.style.borderCollapse = 'separate'; // border-collapseをseparateに設定
+            
+                let tableBody = table.getElementsByTagName('tbody')[0];
                 tableBody.innerHTML = "";
-
+            
                 shelters.sort((a, b) => a.distance - b.distance);
-
+            
                 shelters.forEach(function(shelter) {
                     let row = document.createElement('tr');
-
+                    row.classList.add('your-class-name'); // クラスを追加
+            
+                    // JavaScriptでスタイルを設定
+                    row.style.backgroundColor = '#c0c0c0';
+                    row.style.borderBottom = 'none'; // 行間のボーダーを取り除く
+            
                     let nameCell = document.createElement('td');
                     nameCell.textContent = shelter.name;
+                    nameCell.style.padding = '10px'; // セルにパディングを追加
                     row.appendChild(nameCell);
-
+            
                     let distanceCell = document.createElement('td');
-                    distanceCell.textContent = shelter.distance !== null ? `距離: ${shelter.distance} km` : "取得中";
+                    distanceCell.innerHTML = shelter.distance !== null 
+                    ? `距離:<br><span style="float: right;">${shelter.distance} km</span>` 
+                    : `<span style="float: right;">取得中</span>`;
+                    distanceCell.style.padding = '10px'; // セルにパディングを追加
                     row.appendChild(distanceCell);
-
+            
                     tableBody.appendChild(row);
                 });
             }
+            
+            // この関数を使用してテーブルを初期化または更新する
+            updateTable();
 
             function fetchDistanceAndAzimuth(shelter, city) {
                 // /get_last_locationから取得した都市名の緯度経度を取得
