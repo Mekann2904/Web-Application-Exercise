96bd446b3eced642adc7b0e743a3b1700a63800f
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2024-07-28 00:19:41 +0900

    避難所を見るボタンの処理

--- app/static/js/dbviewer.js
+++ app/static/js/dbviewer.js
@@ -12,10 +12,10 @@ window.onload = function() {
 
             function updateTable() {
                 let table = document.getElementById('shelter-table');
-                table.classList.add('location-table'); // テーブルにクラスを追加
-                table.style.borderSpacing = '0px 5px'; // 行には隙間を開けず、列には5pxの隙間を設定
-                table.style.fontSize = 'larger'; // フォントサイズをlargerに設定
-                table.style.borderCollapse = 'separate'; // border-collapseをseparateに設定
+                table.classList.add('location-table');
+                table.style.borderSpacing = '0px 5px';
+                table.style.fontSize = 'larger';
+                table.style.borderCollapse = 'separate';
             
                 let tableBody = table.getElementsByTagName('tbody')[0];
                 tableBody.innerHTML = "";
@@ -24,43 +24,44 @@ window.onload = function() {
             
                 shelters.forEach(function(shelter) {
                     let row = document.createElement('tr');
-                    row.classList.add('your-class-name'); // クラスを追加
-            
-                    // JavaScriptでスタイルを設定
+                    row.classList.add('your-class-name');
                     row.style.backgroundColor = '#c0c0c0';
-                    row.style.borderBottom = 'none'; // 行間のボーダーを取り除く
+                    row.style.borderBottom = 'none';
             
                     let nameCell = document.createElement('td');
                     nameCell.textContent = shelter.name;
-                    nameCell.style.padding = '10px'; // セルにパディングを追加
+                    nameCell.style.padding = '10px';
                     row.appendChild(nameCell);
             
                     let distanceCell = document.createElement('td');
                     distanceCell.innerHTML = shelter.distance !== null 
                     ? `距離:<br><span style="float: right;">${shelter.distance} km</span>` 
                     : `<span style="float: right;">取得中</span>`;
-                    distanceCell.style.padding = '10px'; // セルにパディングを追加
+                    distanceCell.style.padding = '10px';
                     row.appendChild(distanceCell);
             
                     tableBody.appendChild(row);
                 });
+                
+                // 最も距離が短い避難所名を取得してリンクを更新
+                let closestShelter = shelters.find(shelter => shelter.distance !== null);
+                if (closestShelter) {
+                    let googleMapsLink = document.getElementById('google-maps-link');
+                    googleMapsLink.href = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(closestShelter.name)}`;
+                }
             }
             
-            // この関数を使用してテーブルを初期化または更新する
             updateTable();
 
             function fetchDistanceAndAzimuth(shelter, city) {
-                // /get_last_locationから取得した都市名の緯度経度を取得
                 fetch(`/address_search/${city}`)
                     .then(response => response.json())
                     .then(cityCoords => {
                         console.log("City coordinates:", cityCoords);
-                        // 避難所の緯度経度を取得
                         fetch(`/get_coordinates/${shelter.name}`)
                             .then(response => response.json())
                             .then(shelterCoords => {
                                 console.log("Shelter coordinates for", shelter.name, ":", shelterCoords);
-                                // 距離と方位角を計算
                                 fetch('/calculate_distance', {
                                     method: 'POST',
                                     headers: {
@@ -76,7 +77,7 @@ window.onload = function() {
                                 .then(response => response.json())
                                 .then(result => {
                                     console.log("Distance and azimuths:", result);
-                                    let distanceKm = (parseFloat(result.distance) / 1000).toFixed(1); // メートルからキロメートルに変換し、少数第一位まで丸める
+                                    let distanceKm = (parseFloat(result.distance) / 1000).toFixed(1);
 
                                     if (isNaN(distanceKm) || result.azimuth1 === 'N/A' || result.azimuth2 === 'N/A') {
                                         if (shelter.retries > 0) {
@@ -84,7 +85,7 @@ window.onload = function() {
                                             setTimeout(() => {
                                                 shelter.retries--;
                                                 fetchDistanceAndAzimuth(shelter, city);
-                                            }, 1000); // 1秒待つ
+                                            }, 1000);
                                         } else {
                                             shelter.distance = null;
                                             updateTable();
@@ -101,7 +102,7 @@ window.onload = function() {
                                         setTimeout(() => {
                                             shelter.retries--;
                                             fetchDistanceAndAzimuth(shelter, city);
-                                        }, 100); // 1秒待つ
+                                        }, 1000);
                                     } else {
                                         shelter.distance = null;
                                         updateTable();
@@ -125,7 +126,7 @@ window.onload = function() {
                 .then(response => response.json())
                 .then(data => {
                     console.log("Last location received:", data.location);
-                    const city = data.location;  // 取得した都市名を保存
+                    const city = data.location;
                     shelters.forEach(shelter => fetchDistanceAndAzimuth(shelter, city));
                 })
                 .catch(error => {
